4cc4eb14547e5a626fdc604cd6c921a5e3e72178
PandariV <43098305+PandariV@users.noreply.github.com>
2019-09-12 18:32:32 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+  	var scroll = $(window).scrollTop();
+	  if (scroll > 30) {
+	    $("header").css("background", "black");
+	  }
+	  else{
+		  $("header").css("background", "rgba(255, 255, 255, 0)");  	
+	  }
+  })
+})
