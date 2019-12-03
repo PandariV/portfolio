7f435b8d7feb7a3dabe7fc2ad3f9cd51076bd8de
PandariV <43098305+PandariV@users.noreply.github.com>
2019-12-03 12:11:40 -0600

    updates header thingy

--- js/script.js
+++ js/script.js
@@ -1,11 +1,34 @@
 $(document).ready(function(){
   $(window).scroll(function(){
-  	var scroll = $(window).scrollTop();
-	  if (scroll > 30) {
+	  var scroll = $(window).scrollTop();
+	  //Header
+	  if(scroll > 30) {
 		$("header").css("background", "black");
 	  }
 	  else{
 		$("header").css("background", "rgba(255, 255, 255, 0)");  	
+	  }
+
+	  //Links
+	  if((scroll < innerHeight) && (scroll > 30)) {
+		$(".one").css("color", "gray");
+	  }
+	  else{
+		$(".one").css("color", "white");  	
+	  }
+
+	  if((scroll > innerHeight - 1) && (scroll < innerHeight + 300)) {
+		$(".two").css("color", "gray");
+	  }
+	  else{
+		$(".two").css("color", "white");  	
+	  }
+
+	  if(scroll > innerHeight + 299) {
+		$(".three").css("color", "gray");
+	  }
+	  else{
+		$(".three").css("color", "white");  	
 	  }
   })
 })
