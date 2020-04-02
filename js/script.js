56939c8d738a1dea62bd83290ee97515b5880a47
PandariV <43098305+PandariV@users.noreply.github.com>
2020-04-02 15:21:06 -0500

    change

--- js/script.js
+++ js/script.js
@@ -8,27 +8,5 @@ $(document).ready(function(){
 	  else{
 		$("header").css("background", "rgba(255, 255, 255, 0)");  	
 	  }
-
-	  //Links
-	  if((scroll < innerHeight) && (scroll > 30)) {
-		$(".one").css("color", "gray");
-	  }
-	  else{
-		$(".one").css("color", "white");  	
-	  }
-
-	  if((scroll > innerHeight - 1) && (scroll < innerHeight + 300)) {
-		$(".two").css("color", "gray");
-	  }
-	  else{
-		$(".two").css("color", "white");  	
-	  }
-
-	  if(scroll > innerHeight + 299) {
-		$(".three").css("color", "gray");
-	  }
-	  else{
-		$(".three").css("color", "white");  	
-	  }
   })
 })
